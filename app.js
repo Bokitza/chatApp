9c7ed14dadb6e8aff1f28e283edb80d374321cbd
Yoav Sagi <sagiyoav@gmail.com>
2021-01-20 10:21:26 +0200

    instructions

---
+++ app.js
@@ -0,0 +1,11 @@
+//URL endpoint to server:
+//https://api.airtable.com/v0/apppE0nnnFxJNhCjG/Table%201?api_key=key3AHJQkA3Gkjagf
+
+//Todo - list the messages nicely on the screen
+// (think WhatsApp)
+
+//Todo - issue a POST request to the server, to file a new message every time user push the button
+
+//Todo - ok everything cool... but the screen wont update :/
+//do an automatic fetch every 5 seconds - u can use this function
+//https://www.w3schools.com/jsref/met_win_setinterval.asp
